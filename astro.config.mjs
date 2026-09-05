d5f9d63ec5d2047e974c95bdf3a288cb42c407e9
takeshi-takorva <dkpro8X@gmail.com>
2026-09-05 21:57:10 +0700

    feat: enable Cloudflare SSR for CMS M2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://retroguyvn.com',
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare(),
   trailingSlash: 'never'
 });
